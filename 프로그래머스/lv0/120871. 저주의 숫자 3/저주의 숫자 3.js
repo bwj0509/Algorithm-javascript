6e33c60bc3663b6ed7b87ab30f20a84d14cc8412
WOOJIN BAEK <45286570+bwj0509@users.noreply.github.com>
2023-02-09 20:45:52 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,9 +1,11 @@
 function solution(n) {
   threeXarr = [];
-
-  for (let i = 0; i < 200; i++) {
+  let count = 0;
+  
+  for (let i = 1; count < n; i++) {
     if (i % 3 !== 0 && !i.toString().includes("3")) {
       threeXarr.push(i);
+      count++;
     }
   }
   return threeXarr[n - 1];
