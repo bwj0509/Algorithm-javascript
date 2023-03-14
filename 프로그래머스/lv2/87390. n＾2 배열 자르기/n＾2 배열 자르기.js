31ee5bc04d48fd506ec83c103a120f895c3a7b4c
WOOJIN BAEK <45286570+bwj0509@users.noreply.github.com>
2023-03-14 19:14:01 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.58 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,14 +1,10 @@
 function solution(n, left, right) {
     let result = []
     for(let i=left; i<=right; i++){
-        // console.log(i, '수는',Math.ceil((i+1)/n), '줄에있고', i%n+1, '번째이다.' )
-       
         if(i%n+1 > Math.ceil((i+1)/n)){
-            // console.log(i,'는 if')
             result.push(i%n+1)
         }
         else{
-            // console.log(i,'는 else')
             result.push(Math.ceil((i+1)/n))
         }
         
